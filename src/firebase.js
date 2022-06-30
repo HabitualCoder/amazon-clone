45594e390bd328fd200f7dc8b39ea40b0f295595
HabitualCoder <jaydeep.geekcoder@gmail.com>
2022-06-30 16:09:41 +0530

    updated code

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKjeqvJHxBRP1EewFSsVAO901QsmGBPz0",
-    authDomain: "clone-369f7.firebaseapp.com",
-    projectId: "clone-369f7",
-    storageBucket: "clone-369f7.appspot.com",
-    messagingSenderId: "367550426405",
-    appId: "1:367550426405:web:3a58f6668f2bf616dffecc",
-    measurementId: "G-WCXTCZKB9G"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
